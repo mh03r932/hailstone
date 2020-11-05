c067d4bba3ffd295be2086b9c3cb51113774b826
Markus Hofstetter <markus.hofst@gmail.com>
2020-11-05 21:41:59 +0100

    initial commit

---
+++ hailstone.py
@@ -0,0 +1,79 @@
+
+
+
+def hailstoneStep( number:int):
+    """
+    calculate hailstone
+    """
+    next: int 
+    if number % 2 == 0:
+        
+        next = int(number / 2)
+        
+        return next
+    else:
+        next = number * 3 
+        return next + 1
+        
+    pass
+
+def hailstoneStepFast( number:int, step:int):
+    """
+    calculate hailstone
+    """
+    next: int 
+    if number % 2 == 0:
+        
+        next = int(number / 2)        
+        return (next, (step +1) )
+    else:
+        next = int((number * 3 + 1)  / 2)
+        return (next, (step + 2))
+        
+    pass
+
+
+def runHailstone(num: int, step :int = 1 ):
+    """
+    docstring
+    """
+    currentNum = hailstoneStep(number=num)
+    print('Step  {:>4}  {:>20}'.format(step, num))
+    if(currentNum != 1):
+        runHailstone(currentNum, step  +1)
+    pass
+
+def runHailstoneFast(num: int, stepcount :int = 1 ):
+    """
+    docstring
+    """
+
+    if(num == 1):
+        return stepcount
+    else: 
+        nextNum, nextStepCount = hailstoneStepFast(number=num, step=stepcount)    
+        return runHailstoneFast(nextNum, nextStepCount)
+    pass
+
+
+num, count = hailstoneStepFast(36,0)
+
+
+
+bestum: int
+maxSteps: int = 0 
+for i in range(2, 263728127 ):
+  
+    numOfSteps = runHailstoneFast(i, stepcount=0)    
+    print('n:  {:>4}   {:>20} st'.format(i, numOfSteps))
+    maxSteps = max(maxSteps, numOfSteps)
+pass
+
+print ( maxSteps)
+
+
+
+
+
+# runHailstone(63728127)
+# long = runHailstoneFast(63728127)
