0dba89ef35c8933f7ddc949e85c88bf179a6c8af
Markus Hofstetter <markus.hofst@gmail.com>
2020-11-08 15:14:41 +0100

    code to find number with largest steps bellow 1 000 000 000

--- hailstone.py
+++ hailstone.py
@@ -60,20 +60,25 @@ num, count = hailstoneStepFast(36,0)
 
 
 
-bestum: int
-maxSteps: int = 0 
-for i in range(2, 263728127 ):
+# bestum: int = 0
+# maxSteps: int = 0 
+# for i in range(2, 1000000000 ):
   
-    numOfSteps = runHailstoneFast(i, stepcount=0)    
-    print('n:  {:>4}   {:>20} st'.format(i, numOfSteps))
-    maxSteps = max(maxSteps, numOfSteps)
-pass
+#     numOfSteps = runHailstoneFast(i, stepcount=0)    
+#     print('n:  {:>4}   {:>20} st'.format(i, numOfSteps))
+#     if(maxSteps < numOfSteps):
+#         maxSteps = numOfSteps
+#         bestum = i
+    
+#     maxSteps = max(maxSteps, numOfSteps)
+# pass
 
-print ( maxSteps)
+# print ( bestum, ' ',  maxSteps)
 
 
 
 
 
-# runHailstone(63728127)
+runHailstone(63728127)
+# runHailstone(670617279)
 # long = runHailstoneFast(63728127)
